622e5c1f4b2588b422efbf1b9c3d0655e5fc8052
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-11-09 01:07:21 +0600

    is_prime?

--- 01. Is_Prime?.cpp
+++ 01. Is_Prime?.cpp
@@ -3,16 +3,12 @@
 //Q: -> Is Prime??
 //code:
 //-----
-const int MAXN = 1e6 + 5;
-vector<int> sieve(MAXN, true);
-vector<int> Sieve() {
-    sieve[0] = sieve[1] = false;
-    for (int i = 2; i * i < MAXN; ++i) {
-        if (sieve[i]){
-            for (int j = i * i; j < MAXN; j += i){
-                sieve[j] = false;
-            }
-        }
+bool isprime(int n) {
+    if (n < 2) return false;
+    for(int i = 2; i <= (int)sqrt(n); i++){
+        if (n % i == 0) return false;
     }
-    return sieve;
+    return true;
 }
+
+
